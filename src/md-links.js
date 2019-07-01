dfc96467bc2b22fc14d21afe680ec3046cd9e321
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-07-01 08:03:46 -0400

    se utiliza chalk para dar estilo y funcion md links para imprimir archivos

--- src/md-links.js
+++ src/md-links.js
@@ -2,3 +2,15 @@ const chalk = require('chalk');
 const mdLinks = require("../index");
 const argv = require('yargs').argv;
 
+// argv._[0] es el primer argumento
+// node .\src\md-links.js .\prueba.md
+
+mdLinks(argv._[0])
+  .then(links => {
+    links.forEach(link => {
+        console.log(`${chalk.green(link.file)} ${chalk.yellow(link.href)} ${chalk.magenta(link.text)}`);
+    });
+  })
+  .catch(err => {
+    console.error(chalk.red('Se ha encontrado un error: '), chalk.cyan(err.message));
+  });
